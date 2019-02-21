54e9d655398d1ef1fe2032be3d1eb7bf698d9111
hashzzk <132456@qq.com>
2019-02-21 18:03:40 +0800

    修改/github/acms/application/config/memcached.php

--- application/config/memcached.php
+++ application/config/memcached.php
@@ -5,6 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | -------------------------------------------------------------------------
 | Memcached settings
 | -------------------------------------------------------------------------
+// Opera 10+ always reports Opera/9.80 and appends Version/<real version> to the user agent string
 | Your Memcached servers can be specified below.
 |
 |	See: https://codeigniter.com/user_guide/libraries/caching.html#memcached
