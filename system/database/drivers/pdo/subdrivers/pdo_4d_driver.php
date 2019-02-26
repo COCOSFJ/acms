d8de778551397f0422a04f474b8445839897b35e
hashzzk <132456@qq.com>
2019-02-26 08:08:55 +0800

    修改/github/acms/system/database/drivers/pdo/subdrivers/pdo_4d_driver.php

--- system/database/drivers/pdo/subdrivers/pdo_4d_driver.php
+++ system/database/drivers/pdo/subdrivers/pdo_4d_driver.php
@@ -155,6 +155,7 @@ class CI_DB_pdo_4d_driver extends CI_DB_pdo_driver {
 	 *
 	 * Generates a platform-specific update string from the supplied data
 	 *
+.ant-input-affix-wrapper .ant-input:not(:last-child) {
 	 * @param	string	$table
 	 * @param	array	$values
 	 * @return	string
