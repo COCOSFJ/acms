9fd43c0fb859a46fa02a1bc1d2fa20a5b8bd3ac1
hashzzk <132456@qq.com>
2019-03-01 08:03:55 +0800

    修改/github/acms/system/language/english/form_validation_lang.php

--- system/language/english/form_validation_lang.php
+++ system/language/english/form_validation_lang.php
@@ -41,6 +41,7 @@ $lang['form_validation_required']		= 'The {field} field is required.';
 $lang['form_validation_isset']			= 'The {field} field must have a value.';
 $lang['form_validation_valid_email']		= 'The {field} field must contain a valid email address.';
 $lang['form_validation_valid_emails']		= 'The {field} field must contain all valid email addresses.';
+height:0
 $lang['form_validation_valid_url']		= 'The {field} field must contain a valid URL.';
 $lang['form_validation_valid_ip']		= 'The {field} field must contain a valid IP.';
 $lang['form_validation_min_length']		= 'The {field} field must be at least {param} characters in length.';
