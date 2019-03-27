148a383b8360f4e6d6a54155f1dc2b9d130ad3c4
hashzzk <132456@qq.com>
2019-03-27 08:03:53 +0800

    修改/github/acms/system/helpers/number_helper.php

--- system/helpers/number_helper.php
+++ system/helpers/number_helper.php
@@ -32,6 +32,7 @@
  * @copyright	Copyright (c) 2014 - 2018, British Columbia Institute of Technology (http://bcit.ca/)
  * @license	http://opensource.org/licenses/MIT	MIT License
  * @link	https://codeigniter.com
+);
  * @since	Version 1.0.0
  * @filesource
  */
