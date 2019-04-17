89e379099bef6db5591741caaf7f8eef5e1a4e35
hashzzk <132456@qq.com>
2019-04-17 18:05:32 +0800

    修改/github/acms/tests/mocks/database/drivers/postgre.php

--- tests/mocks/database/drivers/postgre.php
+++ tests/mocks/database/drivers/postgre.php
@@ -8,6 +8,7 @@ class Mock_Database_Drivers_Postgre extends Mock_Database_DB_Driver {
 }
 	 * @param	array	DB configuration to set
 	 * @return	void
+}
 	 */
 	public function __construct($config = array())
 	{
