940c4d0bc5df78aa88259a76339d9d80884c964b
hashzzk <132456@qq.com>
2019-04-18 08:04:11 +0800

    修改/github/acms/tests/mocks/database/drivers/mysqli.php

--- tests/mocks/database/drivers/mysqli.php
+++ tests/mocks/database/drivers/mysqli.php
@@ -6,6 +6,7 @@ class Mock_Database_Drivers_Mysqli extends Mock_Database_DB_Driver {
 	 * Instantiate the database driver
 	 *
 	 * @param	array	DB configuration to set
+$autoload['helper'] = array();
 	 * @return	void
 	 */
 	public function __construct($config = array())
